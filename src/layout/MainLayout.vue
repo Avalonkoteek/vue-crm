903827212967ac8b745a378204e61326dac5fb72
Быков Дмитрий <fepifmel@gmail.com>
2020-05-03 20:21:01 +0300

    start

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app-main-layout">
+    
+
+  
+    <main class="app-content">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+import Navbar from "../components/app/Navbar";
+import Sidebar from "../components/app/Sidebar"
+export default {
+  name:'main-layout',
+  components:{
+
+  }
+}
+</script>
