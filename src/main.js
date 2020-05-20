c17d0273a1fd3cd519620a6e852defda67dc3a28
Быков Дмитрий <fepifmel@gmail.com>
2020-05-20 23:11:32 +0300

    merge

--- src/main.js
+++ src/main.js
@@ -1,11 +1,35 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import dateFilter from './filters/date.filter'
+import messagePlugin from './utils/message.plugin'
+import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+
 Vue.config.productionTip = false
+Vue.use(messagePlugin)
+Vue.use(Vuelidate)
+
+
+
+Vue.filter('date', dateFilter);
+
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyDzQRyuLtUFvezDHcwTAkQvBD1KT_0fONo",
+//   authDomain: "vue-crm-67f17.firebaseapp.com",
+//   databaseURL: "https://vue-crm-67f17.firebaseio.com",
+//   projectId: "vue-crm-67f17",
+//   storageBucket: "vue-crm-67f17.appspot.com",
+//   messagingSenderId: "742318860597",
+//   appId: "1:742318860597:web:86e1cd7398a99a95eb847c"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
 
 new Vue({
   router,
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,56 +1,106 @@
 <template>
-  
-<form class="card auth-card">
-  <div class="card-content">
-    <span class="card-title">Домашняя бухгалтерия</span>
-    <div class="input-field">
-      <input
+  <form class="card auth-card" @submit.prevent="submitHandler">
+    <div class="card-content">
+      <span class="card-title">Домашняя бухгалтерия</span>
+      <div class="input-field">
+        <input
           id="email"
           type="text"
-      >
-      <label for="email">Email</label>
-      <small class="helper-text invalid">Email</small>
-    </div>
-    <div class="input-field">
-      <input
+          v-model.trim="email"
+          :class="{invalid:($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
+        />
+        <label for="email">Email</label>
+        <small
+          class="helper-text invalid"
+          v-if="$v.email.$dirty && !$v.email.required"
+        >Поле email не должно быть пустым</small>
+        <small
+          class="helper-text invalid"
+          v-else-if="$v.email.$dirty && !$v.email.email"
+        >Введите корректный email</small>
+      </div>
+      <div class="input-field">
+        <input
           id="password"
           type="password"
-          class="validate"
-      >
-      <label for="password">Пароль</label>
-      <small class="helper-text invalid">Password</small>
-    </div>
-    <div class="input-field">
-      <input
+          v-model.trim="password"
+          :class="{invalid:($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength)}"
+        />
+        <label for="password">Пароль</label>
+        <small
+          class="helper-text invalid"
+          v-if="$v.password.$dirty && !$v.password.required"
+        >Введите пароль1</small>
+        <small
+          class="helper-text invalid"
+          v-else-if="$v.password.$dirty && !$v.password.minLength"
+        >Минимальная длинна пароля 5 символов</small>
+      </div>
+      <div class="input-field">
+        <input
           id="name"
           type="text"
-          class="validate"
-      >
-      <label for="name">Имя</label>
-      <small class="helper-text invalid">Name</small>
+          v-model.trim="name"
+          :class="{invalid:$v.name.$dirty && !$v.name.required}"
+        />
+        <label for="name">Имя</label>
+        <small
+          v-if="$v.name.$dirty && !$v.name.required"
+          class="helper-text invalid"
+        >Введите ваше имя</small>
+      </div>
+      <p>
+        <label>
+          <input type="checkbox" v-model="agree" />
+          <span>С правилами согласен</span>
+        </label>
+      </p>
     </div>
-    <p>
-      <label>
-        <input type="checkbox" />
-        <span>С правилами согласен</span>
-      </label>
-    </p>
-  </div>
-  <div class="card-action">
-    <div>
-      <button
-          class="btn waves-effect waves-light auth-submit"
-          type="submit"
-      >
-        Зарегистрироваться
-        <i class="material-icons right">send</i>
-      </button>
-    </div>
+    <div class="card-action">
+      <div>
+        <button class="btn waves-effect waves-light auth-submit" type="submit">
+          Зарегистрироваться
+          <i class="material-icons right">send</i>
+        </button>
+      </div>
 
-    <p class="center">
-      Уже есть аккаунт?
-      <a href="/">Войти!</a>
-    </p>
-  </div>
-</form>
+      <p class="center">
+        Уже есть аккаунт?
+        <router-link to="/login">Войти!</router-link>
+      </p>
+    </div>
+  </form>
 </template>
+<script>
+import { email, required, minLength } from "vuelidate/lib/validators";
+export default {
+  name: "register",
+  methods: {
+    submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+      const formData = {
+        email:this.email,
+        password:this.password,
+        name:this.name
+      };
+      console.log(formData)
+      this.$router.push('/')
+    }
+  },
+  data: () => ({
+    email: "",
+    password: "",
+    name: "",
+    agree: false
+  }),
+  validations: {
+    email: { email, required },
+    password: { required, minLength: minLength(6) },
+    name: { required },
+    agree: { checked: v => v }
+  }
+};
+</script>
